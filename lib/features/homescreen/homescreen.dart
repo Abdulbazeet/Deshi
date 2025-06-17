64e0467e544950cf01af91ca0646891f7c7b909c
Abduulbzeet <abdulbazeetbalogun4@gmail.com>
2025-06-17 14:27:41 +0100

    `Added navigation and UI components to HomeScreen and Shop features`

--- lib/features/homescreen/homescreen.dart
+++ lib/features/homescreen/homescreen.dart
@@ -1,3 +1,9 @@
+import 'package:deshi_mart/core/appColors.dart' show AppColors;
+import 'package:deshi_mart/features/account/account.dart';
+import 'package:deshi_mart/features/cart/cart.dart';
+import 'package:deshi_mart/features/explore/explore.dart';
+import 'package:deshi_mart/features/favourite/favourite.dart';
+import 'package:deshi_mart/features/shop/shop.dart';
 import 'package:flutter/material.dart';
 
 class HomeScren extends StatefulWidget {
@@ -8,8 +14,60 @@ class HomeScren extends StatefulWidget {
 }
 
 class _HomeScrenState extends State<HomeScren> {
+  final List _body = [Shop(), Explore(), Cart(), Favourite(), Account()];
+  int _index = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _body[_index],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          selectedItemColor: AppColors.mainColor,
+          unselectedItemColor: Colors.black,
+
+          onTap: (value) {
+            setState(() {
+              _index = value;
+            });
+          },
+          showUnselectedLabels: true,
+
+          currentIndex: _index,
+
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.storefront_outlined),
+              activeIcon: Icon(Icons.storefront),
+              label: 'Shop',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.manage_search_outlined),
+              activeIcon: Icon(Icons.manage_search_sharp),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_outlined),
+              activeIcon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline_rounded),
+              activeIcon: Icon(Icons.favorite),
+              label: 'Favourite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_2_outlined),
+              activeIcon: Icon(Icons.person),
+              label: 'Account',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/shop/shop.dart
+++ lib/features/shop/shop.dart
@@ -1,4 +1,7 @@
+import 'package:deshi_mart/core/appColors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
 class Shop extends StatefulWidget {
   const Shop({super.key});
@@ -10,6 +13,78 @@ class Shop extends StatefulWidget {
 class _ShopState extends State<Shop> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.sp),
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              actionsPadding: EdgeInsets.all(0),
+              title: Row(
+                children: [
+                  Image.asset('assets/Group.png', width: 25.sp, height: 25.sp),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.location_on),
+                        SizedBox(width: 5.sp),
+                        Text(
+                          'Ikeja, Lagos',
+                          style: GoogleFonts.poppins(
+                            fontSize: 15.sp,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              pinned: true,
+              bottom: PreferredSize(
+                preferredSize: Size(100.w, 34.sp),
+
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15.sp),
+                  height: 30.sp,
+
+                  alignment: Alignment.centerLeft,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withValues(alpha: .2),
+                    borderRadius: BorderRadius.circular(15.sp),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration.collapsed(
+                            hintText: 'Search store',
+                            hintStyle: GoogleFonts.poppins(fontSize: 14.sp),
+                          ),
+                        ),
+                      ),
+
+                      Icon(Icons.search, color: AppColors.mainColor),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  SizedBox(height: 20.sp),
+                  Container(height: 30.sp, width: 100.w, color: Colors.grey),
+                ],
+              ),
+            ),
+          ],
+
+          // SizedBox(height: 25.sp),
+        ),
+      ),
+    );
   }
 }
